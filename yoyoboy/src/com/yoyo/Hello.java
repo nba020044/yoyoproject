2b352976beb842270773cc6b08bdf4d8065b5c18
user <user@es302n20>
2018-09-25 12:03:36 +0800

    initial

---
+++ yoyoboy/src/com/yoyo/Hello.java
@@ -0,0 +1,14 @@
+package com.yoyo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int age = 10;
+		age = 12;
+		short num = 8;
+				age = num;
+		float weight = 52.5f;
+		System.out.println(weight);
+	}
+
+}
